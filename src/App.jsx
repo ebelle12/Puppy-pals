208811749d9e7b758b1b157eca8e3f72d042b972
Eva Belle <evabelle@Evas-Air.lan>
2024-03-17 19:40:28 -0500

    2nd push

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import { puppyList } from './data.js'
 import { useState } from 'react'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  const [featPupId, setFeatPupId] = useState(null)
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(puppies);
+
+  function handleClick() {
+    // some logic here
+  }
 
   return (
-    <div>
-      {
-        puppies.map((puppy) => {
-          return <p onClick={()=>{}} key={puppy.id}>{puppy.name}</p>
-        })
-      }
-
-
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>}
     </div>
   )
 }
